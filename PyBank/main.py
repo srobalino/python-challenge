8a32013c490656d28aeee7db3a44373e3b0d3359
Stephanie Robalino <srobalino23@gmail.com>
2020-02-17 20:21:11 -0500

    Financial Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@
 import os
 import csv
 
-#setst he location of the csv file 
+#sets the location of the csv file 
 budget_data = os.path.join('..','PyBank','budget_data.csv')
 
 #defined variables, I set all to zero to later declare it as a string if needed. 
@@ -36,26 +36,32 @@ with open (budget_data) as csvfile:
     if int(row[1]) < gr_decrease:
         gr_decrease = int(row[1])    
         gr_mn_decrease= str(row[0])
+
 #print to terminal, did not set to F string as I wanted to test defining integers and string 
     print("Financial Analysis:")
     print("---------------------------------")
     print("Total Months:", int(count_of_row))
-    print("Total Profit/Losses:", int(profit_total))
-    print("Total Average:", int(avg_total))
-    print("Total Months Increase:", int(gr_increase) , str(gr_mn_increase))    
-    print("Total Months Decrease:", int(gr_decrease) , str(gr_mn_decrease))  
+    print("Total Profit/Losses:", "$" ,int(profit_total))
+    print("Total Average:", "$", int(avg_total))
+    print("Total Months Increase:", "$", int(gr_increase) , str(gr_mn_increase))    
+    print("Total Months Decrease:","$", int(gr_decrease) , str(gr_mn_decrease))  
 
 #print to a text file 
 #create the file and specify the Path of the file 
 bgt_data = os.path.join ("..", "PyBank", "print_buget_data.txt")
 #open the file and set it to write. 
-file=open(bgt_data, 'w')
+file=open(bgt_data, 'w', encoding ='utf-8')
 #Define what output to print to the TXT file 
 file.write(f"Financial Analysis \n")
 file.write(f'------------------------------------ \n')
 file.write(f'Total Months: {count_of_row} \n')
-file.write(f'Total Profit/Losses: {profit_total} \n')
-file.write(f'{avg_total} \n')
-file.write(f"Total Increase: {(gr_increase)} {(gr_mn_increase)} \n")  
-file.write(f'Total Decrease: {(gr_decrease)} {(gr_mn_decrease)} \n')
+file.write(f'Total Profit/Losses: ${profit_total} \n')
+file.write(f'${avg_total} \n')
+file.write(f"Total Increase: ${(gr_increase)} {(gr_mn_increase)} \n")  
+file.write(f'Total Decrease: ${(gr_decrease)} {(gr_mn_decrease)} \n')
 file.close
+
+
+#ref site https://dfrieds.com/python/read-in-csv-files.html   https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python 
+#https://www.programiz.com/python-programming/file-operation
+#
